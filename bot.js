e60e96bcbc050d7308df3c4bf7ce2e5db3ce5cc6
PhiphiAuThon <32982765+PhiphiAuThon@users.noreply.github.com>
2017-10-21 21:40:06 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,62 +1,14 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var func = require('./functions');
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-const PREFIX = '<@370238406820888577>';
-
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-
-// Initialize Discord Bot
-var bot = new Discord.Client();
-
-bot.on('ready', function (evt) {
-    console.log('Momo is ready!');
+client.on('ready',() =>{
+	console.log('Momo is ready!');
 });
 
-bot.on('message', function (user, userID, channelID, message, evt) {
-/*if (message.substring(0, 1) == '!') { }*/
-/*var args = message.content.substring(PREFIX.length).split(" "); */
- 	message = message.split(' ');
-    if(message[0].toLowerCase() != PREFIX.toLowerCase()) return;
-
-    var message_index = 0;
-    for (i = 0; i < message.length; i++) {
-    	switch (message[i].toLowerCase()){
-    		case 'panini': 
-    			message_index = 1;
-    			break;
-    		case 'mixtape':
-    			message_index = 2;
-    			break;
-    		case 'yare':
-    			message_index = 3;
-    			break;
-    		case 'hana':
-    			message_index = 3;
-    			break;
-    		case 'pense':
-    			message_index = 4;
-    			break;
-    		case 'penses':
-    			message_index = 4;
-    			break;
-    		default:
-    			break;
-    	}
-    	// A garder?
-    	//if(message_index != 0)
-    	//	break;
-    }
-
-    answer = func.give_answer(message_index,user);
-
-    bot.sendMessage({to: channelID,message: answer});
-
+client.on('message', message =>{
+	if(message.content === 'ping'){
+		message.reply('pong');
+	}
 });
 
-bot.loggin(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);

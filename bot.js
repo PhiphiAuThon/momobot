e96d7a9385627534482376e1c22584dc83406850
PhiphiAuThon <32982765+PhiphiAuThon@users.noreply.github.com>
2017-10-21 22:00:57 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -10,13 +10,45 @@ bot.on('ready',() =>{
 
 bot.on('message', function(user, userID, channelID, message, evt) {
 
-	var answer ;//= //func.give_answer(message_index,user);
+	message = message.split(' ');
+    if(message[0].toLowerCase() != PREFIX.toLowerCase()) return;
 
-	if(message.content === 'ping'){
-		answer = 'pang';
+    var message_index = 0;
+    for (i = 0; i < message.length; i++) {
+    	switch (message[i].toLowerCase()){
+    		case 'panini': 
+    			message_index = 1;
+    			break;
+    		case 'mixtape':
+    			message_index = 2;
+    			break;
+    		case 'yare':
+    			message_index = 3;
+    			break;
+    		case 'hana':
+    			message_index = 3;
+    			break;
+    		case 'pense':
+    			message_index = 4;
+    			break;
+    		case 'penses':
+    			message_index = 4;
+    			break;
+    		default:
+    			break;
+    	}
+    	// A garder?
+    	//if(message_index != 0)
+    	//	break;
+    }
+
+	//var answer ;//= //func.give_answer(message_index,user);
+
+	if(message[1] === 'ping'){
+		message.reply(); //answer = 'pang';
 	}
 
-    bot.sendMessage({to: channelID,message: answer});
+    //bot.sendMessage({to: channelID,message: answer});
 });
 
 bot.login(process.env.BOT_TOKEN);

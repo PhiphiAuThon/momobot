13e0e8760457ac3c848c28f455ba6218db531808
PhiphiAuThon <32982765+PhiphiAuThon@users.noreply.github.com>
2017-10-21 22:13:08 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -8,7 +8,42 @@ bot.on('ready',() =>{
 	console.log('Momo is ready!');
 });
 
-bot.on('message', function(user, userID, channelID, message, evt) {
+bot.on('message', function(message) {
+
+	var msg = message.split(' ');
+    if(msg[0].toLowerCase() != PREFIX.toLowerCase()) return;
+
+    var message_index = 0;
+    for (i = 0; i < msg.length; i++) {
+    	switch (msg[i].toLowerCase()){
+    		case 'panini': 
+    			message_index = 1;
+    			break;
+    		case 'mixtape':
+    			message_index = 2;
+    			break;
+    		case 'yare':
+    			message_index = 3;
+    			break;
+    		case 'hana':
+    			message_index = 3;
+    			break;
+    		case 'pense':
+    			message_index = 4;
+    			break;
+    		case 'penses':
+    			message_index = 4;
+    			break;
+    		default:
+    			break;
+    	}
+    	// A garder?
+    	//if(message_index != 0)
+    	//	break;
+    }
+
+   // answer = func.give_answer(message_index,user);
+
 	if(message.content === 'ping'){
 		message.reply('pong');
 	}

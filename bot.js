ccec7fbfdf77fe8afefaa075575209d30797963a
PhiphiAuThon <32982765+PhiphiAuThon@users.noreply.github.com>
2017-10-21 21:56:21 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -8,10 +8,15 @@ bot.on('ready',() =>{
 	console.log('Momo is ready!');
 });
 
-bot.on('message', function(message) {
+bot.on('message', function(user, userID, channelID, message, evt) {
+
+	var answer ;//= //func.give_answer(message_index,user);
+
 	if(message.content === 'ping'){
-		message.reply('pong');
+		answer = 'pang';
 	}
+
+    bot.sendMessage({to: channelID,message: answer});
 });
 
 bot.login(process.env.BOT_TOKEN);

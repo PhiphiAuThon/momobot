f53370ddbba3eb395f12826f52742a6b29a17bb6
PhiphiAuThon <32982765+PhiphiAuThon@users.noreply.github.com>
2017-10-21 21:50:06 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,14 +1,52 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const bot = new Discord.Client();
+var func = require('./functions');
 
-client.on('ready',() =>{
+const PREFIX = '<@370238406820888577>';
+
+bot.on('ready',() =>{
 	console.log('Momo is ready!');
 });
 
-client.on('message', message =>{
-	if(message.content === 'ping'){
-		message.reply('pong');
-	}
+bot.on('message',  function (user, userID, channelID, message, evt) {
+/*if (message.substring(0, 1) == '!') { }*/
+/*var args = message.content.substring(PREFIX.length).split(" "); */
+ 	message = message.split(' ');
+    if(message[0].toLowerCase() != PREFIX.toLowerCase()) return;
+
+    var message_index = 0;
+    for (i = 0; i < message.length; i++) {
+    	switch (message[i].toLowerCase()){
+    		case 'panini': 
+    			message_index = 1;
+    			break;
+    		case 'mixtape':
+    			message_index = 2;
+    			break;
+    		case 'yare':
+    			message_index = 3;
+    			break;
+    		case 'hana':
+    			message_index = 3;
+    			break;
+    		case 'pense':
+    			message_index = 4;
+    			break;
+    		case 'penses':
+    			message_index = 4;
+    			break;
+    		default:
+    			break;
+    	}
+    	// A garder?
+    	//if(message_index != 0)
+    	//	break;
+    }
+
+    answer = func.give_answer(message_index,user);
+
+    bot.sendMessage({to: channelID,message: answer});
+
 });
 
-client.login(process.env.BOT_TOKEN);
+bot.login(process.env.BOT_TOKEN);
